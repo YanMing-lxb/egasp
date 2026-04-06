1c660f4776492adb209f3f6128e97fd91b6312c1
YanMing-CX <lxb-yanming@foxmail.com>
2026-04-06 10:25:15 +0800

    docs(tools/config): 添加文件头注释信息
    添加koroFileHeader自动生成的文件头注释，包含作者信息、最后编辑时间和文件描述等元数据

--- tools/config.py
+++ tools/config.py
@@ -1,4 +1,12 @@
 '''
+Author: YanMing-CX lxb-yanming@foxmail.com
+Date: 2026-04-04 18:44:47
+LastEditors: YanMing-CX lxb-yanming@foxmail.com
+LastEditTime: 2026-04-04 21:28:10
+FilePath: \egasp\tools\config.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+'''
  =======================================================================
  ····Y88b···d88P················888b·····d888·d8b·······················
  ·····Y88b·d88P·················8888b···d8888·Y8P·······················
